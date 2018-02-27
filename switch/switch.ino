8f895cc207bd4adca92516e8ec078f6dcc0f523c
fenwickelliott <charles@fenwickelliott.io>
2018-02-27 12:04:01 -0500

    swithcing states, (not printing)

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,27 +1,33 @@
+int status = 0;
+
 void setup() {
+//  Serial.begin(9600);
   pinMode(1, INPUT);
   pinMode(2, INPUT);
   pinMode(3, INPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
+//  Serial.println("0: stopped");
 }
 
 void loop() {
-  if (digitalRead(1) == 1) {
+  if (digitalRead(1) == HIGH && status != 1) {
+//    Serial.println("1: running");
     digitalWrite(11, HIGH);
-  } else {
-    digitalWrite(11, LOW);
-  }
-  if (digitalRead(2) == 1) {
-    digitalWrite(12, HIGH);
-  } else {
     digitalWrite(12, LOW);
-  }
-  if (digitalRead(3) == 1) {
-    digitalWrite(13, HIGH);
-  } else {
     digitalWrite(13, LOW);
   }
+  if (digitalRead(2) == HIGH && status != 2) {
+//    Serial.println("2: ramping down");
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+  }
+  if (digitalRead(3) == HIGH && status != 3) {
+//    Serial.println("3: stopping");
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(13, HIGH);
+  }
 }
-
