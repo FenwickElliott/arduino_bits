87f094648439acc2b232cd0afd6d97662d870105
fenwickelliott <charles@fenwickelliott.io>
2018-02-27 10:04:07 -0500

    two switches, two leds

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,15 +1,29 @@
 void setup() {
-  Serial.begin(9600);
+//  Serial.begin(9600);
+//  pinMode(2, INPUT);
+//  pinMode(13, OUTPUT);
+//  Serial.println("setup");
+  pinMode(1, INPUT);
   pinMode(2, INPUT);
-  pinMode(10, OUTPUT);
-  Serial.println("setup");
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
-   if (digitalRead(2) == 1) {
-    digitalWrite(10, HIGH);
-   } else {
-    digitalWrite(10, LOW);
-   }
+//   if (digitalRead(2) == 1) {
+//    digitalWrite(13, HIGH);
+//   } else {
+//    digitalWrite(13, LOW);
+//   }
+  if (digitalRead(1) == 1) {
+    digitalWrite(11, HIGH);
+  } else {
+    digitalWrite(11, LOW);
+  }
+    if (digitalRead(2) == 1) {
+    digitalWrite(12, HIGH);
+  } else {
+    digitalWrite(12, LOW);
+  }
 }
 
