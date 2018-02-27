2b9df3a7c0619a4b00a363e42fad15547058e9b1
fenwickelliott <charles@fenwickelliott.io>
2018-02-27 10:12:23 -0500

    three switches, three leds

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,29 +1,27 @@
 void setup() {
-//  Serial.begin(9600);
-//  pinMode(2, INPUT);
-//  pinMode(13, OUTPUT);
-//  Serial.println("setup");
   pinMode(1, INPUT);
   pinMode(2, INPUT);
+  pinMode(3, INPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-//   if (digitalRead(2) == 1) {
-//    digitalWrite(13, HIGH);
-//   } else {
-//    digitalWrite(13, LOW);
-//   }
   if (digitalRead(1) == 1) {
     digitalWrite(11, HIGH);
   } else {
     digitalWrite(11, LOW);
   }
-    if (digitalRead(2) == 1) {
+  if (digitalRead(2) == 1) {
     digitalWrite(12, HIGH);
   } else {
     digitalWrite(12, LOW);
+  }
+  if (digitalRead(3) == 1) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
   }
 }
 
