9e3f7fbff8f6e4f50ef7141e082fa7c3b2421f4d
fenwickelliott <charles@fenwickelliott.io>
2018-02-27 18:05:49 -0500

    declaring and logging state

--- main/main.ino
+++ main/main.ino
@@ -1,18 +1,22 @@
 int i;
+int state = 2;
 
 int SW[] = {2,3,4};
 int LED[] = {10, 11, 12};
 
 void setup() {
+  Serial.begin(9600);
   for(i = 0; i < sizeof(SW) / sizeof(int); i++) {
     pinMode(SW[i], INPUT);
   }
   for(i = 0; i < sizeof(LED) / sizeof(int); i++) {
     pinMode(LED[i], OUTPUT);
-  }    
+  }
+  Serial.println("Set up complete!");
+  Serial.print("State: ");
+  Serial.println(state);
 }
 
 void loop() {
   
 }
-
