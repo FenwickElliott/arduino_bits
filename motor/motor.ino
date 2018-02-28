02693421dac423c7a5699a86605f20d33a5194bc
fenwickelliott <charles@fenwickelliott.io>
2018-02-27 19:17:22 -0500

    Liniar run, ramp, stop

---
+++ motor/motor.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *M1 = AFMS.getMotor(1);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Setting up motor control");
+  AFMS.begin();
+
+  M1->setSpeed(255);
+  M1->run(FORWARD);
+//  M1->setSpeed(255);
+  M1->run(RELEASE);
+}
+
+void loop() {
+  uint8_t i;
+  Serial.println("Waiting for start signal... (3 seconds)");
+  delay(3000);
+  Serial.println("Starting phase 0: Running");
+  M1->run(FORWARD);
+  M1->setSpeed(255);
+  delay(5000);
+  Serial.println("Starting phase 1: Ramping down");
+  for(i = 254; i > 0; i--) {
+    M1->setSpeed(i);
+    delay(50);
+  }
+  delay(100000);
+}
