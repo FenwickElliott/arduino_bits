d1a567f8ccbdce02888a404a43ccf44c4a8e62fa
fenwickelliott <charles@fenwickelliott.io>
2018-02-27 19:34:12 -0500

    functions constructed

--- motor/motor.ino
+++ motor/motor.ino
@@ -8,20 +8,37 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Setting up motor control");
   AFMS.begin();
+
+  Serial.println("Waiting for start signal... (2 seconds)");
+  delay(2000);
+  startUp();
+  delay(5000);
+  rampDown();
+  finalStop();
+  Serial.println("Demonstration complete");
 }
 
 void loop() {
-  uint8_t i;
-  Serial.println("Waiting for start signal... (3 seconds)");
-  delay(3000);
+//  Serial.println("Waiting for start signal... (3 seconds)");
+}
+
+void startUp() {
   Serial.println("Starting phase 0: Running");
   M1->run(FORWARD);
   M1->setSpeed(255);
-  delay(5000);
+}
+
+void rampDown() {
   Serial.println("Starting phase 1: Ramping down");
-  for(i = 254; i > 0; i--) {
+  uint8_t i;
+  for(i = 254; i > 200; i--) {
     M1->setSpeed(i);
     delay(50);
   }
-  delay(100000);
+}
+
+void finalStop() {
+  Serial.println("Starting phase 2: complete rest");
+  M1->run(FORWARD);
+  M1->setSpeed(0);
 }
