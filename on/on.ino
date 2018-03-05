68572fa2dc3c3c147f89f8c27c34edd600291aba
fenwickelliott <charles@fenwickelliott.io>
2018-03-05 16:52:35 -0500

    All Pins outputting HIGH

--- on/on.ino
+++ on/on.ino
@@ -1,8 +1,10 @@
-int pinNum = 13; // pin 13 illuminates LED
+int i;
 
 void setup() {
-  pinMode(pinNum, OUTPUT);
-  digitalWrite(pinNum, HIGH);
+  for(i=0; i <= 13; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, HIGH);
+  }
 }
 
 void loop() {
